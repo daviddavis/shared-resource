279d2948508cb093e2ba9a92a1db1ed54e7c2a8b
David Davis <davidd@scimedsolutions.com>
2012-07-27 12:08:47 -0400

    Added datomic to project

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject shared-resource "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.3.0-beta3"]]
+                           [noir "1.3.0-beta3"]
+                           [com.datomic/datomic-free "0.8.3343"]]
             :main shared-resource.server)
 
