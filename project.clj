31d6d2854c0f582d0535c00ad5d59c7eeef1a2f3
Mike Simpson <mike.simpson@scimedsolutions.com>
2012-07-27 12:53:38 -0400

    Add emacs support

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
                            [com.datomic/datomic-free "0.8.3343"]]
+            :plugins [[lein-swank "1.4.4"]]
             :main shared-resource.server)
 
