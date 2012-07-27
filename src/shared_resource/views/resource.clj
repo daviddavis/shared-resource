7f46be4c83de954614ed05525ab5fc1d6b605ca7
Mike Simpson <mike.simpson@scimedsolutions.com>
2012-07-27 14:50:26 -0400

    Resource view works

--- src/shared_resource/views/resource.clj
+++ src/shared_resource/views/resource.clj
@@ -1,33 +1,43 @@
 (ns shared-resource.views.resource
   (:use noir.core
         hiccup.core
-        hiccup.page)
+        hiccup.page
+        hiccup.element)
   (:require [shared-resource.models.resource :as resource]
             [shared-resource.views.common :as common]
             [noir.response :as resp]))
 
 ;; Partials
 
-(defpartial resource-list [{:keys [perma-link name description start-time end-time] :as resource-item}]
-            (when resource-item
-              [:li.resource-item
-               ;[:h2 (link-to perma-link name)]
-               [:ul.time
-                [:li start-time]
-                [:li end-time]
-                ;;(when (user/admin?)
+(defpartial resource-list [
+                           {:keys [
+                                   perma-link
+                                   name
+                                   description
+                                   start-time
+                                   end-time
+                                   ]
+                            :as resource-item}
+                           ]
+  (when resource-item
+    [:li.resource-item
+     [:h2 (link-to perma-link name)]
+     [:div.content description]
+     [:ul.time
+      [:li start-time]
+      [:li end-time]
+      ;;(when (user/admin?)
                                         ;[:li (link-to (resource/edit-url post) "edit")])
-                ]
-               [:div.content description]]))
+      ]
+     ]))
 
 (defpartial resources-page [resources]
-            (common/layout
-              [:ul.resources
-               (map resource-list resources)]
-              ))
+  (common/layout
+   [:ul.resources
+    (map resource-list resources)]
+   ))
 
 ;; Page structure
 
 (defpage "/" []
-  (html (str (resource/get-all))))
-
+  (resource-list resource/get-all))
