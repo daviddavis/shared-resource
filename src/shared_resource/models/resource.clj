7b9b12e84d0065fedb24e4404b934cc68122b8e6
Mike Simpson <mike.simpson@scimedsolutions.com>
2012-07-29 18:58:08 -0400

    Pull data from the database into the model

--- src/shared_resource/models/resource.clj
+++ src/shared_resource/models/resource.clj
@@ -6,20 +6,11 @@
             [clojure.string :as string]
             ;[shared-resource.models.user :as users]
             [noir.validation :as validation]
-            [noir.session :as session])
+            [noir.session :as session]
+            [shared-resource.datomic :as db])
   )
 
-;(def date-format (tform/formatter "MM/dd/yy" (ctime/default-time-zone)))
-;(def time-format (tform/formatter "h:mma" (ctime/default-time-zone)))
-(defn now [] (new java.util.Date))
 
 
-(def get-all
-  {
-   :perma-link "http://google.com"
-   :name "Awesome Resource"
-   :description "Too awesome to describe."
-   :start-time (now)
-   :end-time (now)
-   }
-  )
+(defn get-all []
+  (map (partial zipmap '(:id :name :description)) (db/get-all-resources))) 
