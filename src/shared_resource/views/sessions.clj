f4a8ae18cad578721e384835e9b920febdd55e12
David Davis <davidd@scimedsolutions.com>
2012-08-04 17:25:31 -0400

    Got authentication pages working

---
+++ src/shared_resource/views/sessions.clj
@@ -0,0 +1,51 @@
+(ns shared-resource.views.sessions
+  (:use noir.core
+        hiccup.core
+        hiccup.page
+        hiccup.element
+        hiccup.form)
+  (:require [shared-resource.models.user :as user]
+            [shared-resource.views.common :as common]
+            [noir.response :as resp]
+            [noir.session :as session]))
+
+(defpartial login-page [username]
+  (common/layout
+    (form-to [:post "/sessions"]
+      (label "username" "Username: ")
+      (text-field "username" username)
+      (label "password" "Password: ")
+      (password-field "password")
+      (submit-button "Login"))))
+
+(defn logged-in? []
+  (not (nil? (session/get :username))))
+
+;; Routes
+
+(pre-route "/" {}
+  (when-not (logged-in?)
+    (resp/redirect "/sessions/new")))
+
+(pre-route "/resources/*" {}
+  (when-not (logged-in?)
+    (resp/redirect "/sessions/new")))
+
+(defpage "/sessions/new" []
+  (login-page ""))
+
+(defpage [:post "/sessions"] {:keys [username password]}
+  (if (user/login? username password)
+    (do
+      (session/put! :username username)
+      (session/flash-put! :success "Successfully logged in.")
+      (resp/redirect "/"))
+    (do
+      (session/flash-put! :error "Failed to log in.")
+      (login-page username))))
+
+(defpage "/sessions/destroy" []
+  (do
+    (session/clear!)
+    ;; (session/flash-put! :success "Logged out.") can't session/clear to work with this
+    (resp/redirect "/sessions/new")))
