86d833ae318c74d8e49dd1cd80c48732a39a237a
David Davis <davidd@scimedsolutions.com>
2012-08-04 13:25:40 -0400

    Added user email to database and shutdown call
    Added user email to database and added shutdown-agents to the create-db
    script.

--- src/shared_resource/datomic.clj
+++ src/shared_resource/datomic.clj
@@ -30,6 +30,12 @@
          :db/valueType :db.type/string
          :db/cardinality :db.cardinality/one
          :db/doc "A user's full name"
+         :db.install/_attribute :db.part/db}
+        {:db/id #db/id[:db.part/db]
+         :db/ident :user/email
+         :db/valueType :db.type/string
+         :db/cardinality :db.cardinality/one
+         :db/doc "A user's email address"
          :db.install/_attribute :db.part/db}])
 
       ;; schema for resources
