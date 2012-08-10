cbdfaff2035a370aa9dec581ea5141973f8f9f7f
David Davis <ddavis1@gmail.com>
2012-08-10 16:32:23 -0400

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject shared-resource "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.3.0-beta3"]
-                           [com.datomic/datomic-free "0.8.3343"]
+                           [noir "1.3.0-beta10"]
+                           [com.datomic/datomic-free "0.8.3397"]
                            [org.clojars.pntblnk/clj-ldap "0.0.9"]]
             :plugins [[lein-swank "1.4.4"]]
             :main shared-resource.server)
