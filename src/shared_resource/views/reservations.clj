a0690c909786922853487d1b35b9677ef9c0ba96
David Davis <davidd@scimedsolutions.com>
2012-08-11 19:51:53 -0400

    Created a reservations new page

--- src/shared_resource/views/reservations.clj
+++ src/shared_resource/views/reservations.clj
@@ -18,3 +18,20 @@
 
 (defpartial reservation-list [reservations]
   [:div.reservations (map reservation-div reservations)])
+
+(defpartial new-reservation-page [start end resource]
+  [:div.new-reservation])
+
+;; Routes
+
+(defpage "/reservations/new" []
+  (new-reservation-page "" "" ""))
+
+(defpage [:post "/reservations"] {:keys [start end resource]}
+  (if (reservation/create-reservation resource (session/get :user) start end)
+    (do
+      (session/flash-put! :success "Successfully create reservation.")
+      (resp/redirect (str "/resources/" resource)))
+    (do
+      (session/flash-put! :error "Failed to log in.")
+      (new-reservation-page start end resource))))
