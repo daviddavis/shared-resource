d0d9944cb11709e720a240041ef0a56377c5cd98
David Davis <davidd@scimedsolutions.com>
2012-08-11 18:23:12 -0400

    Reverting cljs changes to src/shared_resource/server.clj

--- src/shared_resource/server.clj
+++ src/shared_resource/server.clj
@@ -1,14 +1,11 @@
 (ns shared-resource.server
-  (:require [noir.server :as server]
-            [noir.cljs.core]))
+  (:require [noir.server :as server]))
 
-(def cljs-options {:advanced {:externs ["externs/jquery.js"]}})
 (server/load-views-ns 'shared-resource.views)
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
-    (noir.cljs.core/start mode cljs-options)
     (server/start port {:mode mode
                         :ns 'shared-resource})))
 
