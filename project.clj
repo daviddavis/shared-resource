13e690692c2debdde1e03d0a6542e1f561fe997e
David Davis <davidd@scimedsolutions.com>
2012-08-11 18:18:46 -0400

    Merge branch 'develop' of github.com:SciMed/shared-resource into develop
    Fixed depedencies and code conflicts. Got rid of noir-cljs in favor of
    lein-cljsbuild.
    
    Conflicts:
    	src/shared_resource/views/resource.clj

--- project.clj
+++ project.clj
@@ -2,8 +2,9 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta10"]
+                           [lein-cljsbuild "0.2.5"]
                            [com.datomic/datomic-free "0.8.3397"]
                            [org.clojars.pntblnk/clj-ldap "0.0.9"]]
             :plugins [[lein-swank "1.4.4"]]
-            :main shared-resource.server)
+            :main ^{:skip-aot true} shared-resource.server)
 
